a863764f80478f869d8bf8e309a28a04f845386c
Eric Webber <ewebber283@gmail.com>
2022-03-21 18:22:52 -0500

    tested fetch with repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-getUserRepos = () => {
-    fetch("https://api.github.com/users/octocat/repos");
+getUserRepos = (user) => {
+
+    const apiUrl = 'https://api.github.com/users/' + user + '/repos';
+    //extra then is due to promise being returned
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        })
+    });  
 };
 
 getUserRepos();
